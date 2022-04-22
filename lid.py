8519a2eb1852403c52c6629ed0715065672a45f0
Shamimgardobuya <shamimobuya@gmail.com>
2022-04-22 06:59:38 +0300

    dictionaries

---
+++ lid.py
@@ -0,0 +1,123 @@
+python3
+Python 3.8.10 (default, Mar 15 2022, 12:22:08) 
+[GCC 9.4.0] on linux
+Type "help", "copyright", "credits" or "license" for more information.
+>>> a=[]
+>>> a=input("Enter a number")
+Enter a number3,4,5
+>>> a
+'3,4,5'
+>>> >>> bacons=["dogs","moose","cats"]
+  File "<stdin>", line 1
+    >>> bacons=["dogs","moose","cats"]
+    ^
+SyntaxError: invalid syntax
+>>> >>> spam=["cats","dogs","moose"]
+  File "<stdin>", line 1
+    >>> spam=["cats","dogs","moose"]
+    ^
+SyntaxError: invalid syntax
+>>> >>> spam==bacon
+  File "<stdin>", line 1
+    >>> spam==bacon
+    ^
+SyntaxError: invalid syntax
+>>> Traceback (most recent call last):
+  File "<stdin>", line 1
+    Traceback (most recent call last):
+                    ^
+SyntaxError: invalid syntax
+>>>   File "<stdin>", line 1, in <module>
+  File "<stdin>", line 1
+    File "<stdin>", line 1, in <module>
+    ^
+IndentationError: unexpected indent
+>>> NameError: name 'bacon' is not defined
+  File "<stdin>", line 1
+    NameError: name 'bacon' is not defined
+                    ^
+SyntaxError: invalid syntax
+>>> >>> spam==bacons
+  File "<stdin>", line 1
+    >>> spam==bacons
+    ^
+SyntaxError: invalid syntax
+>>> False
+False
+>>> >>>  eggs = {'name': 'Zophie', 'species': 'cat', 'age': '8'}
+  File "<stdin>", line 1
+    >>>  eggs = {'name': 'Zophie', 'species': 'cat', 'age': '8'}
+    ^
+SyntaxError: invalid syntax
+>>>   File "<stdin>", line 1
+  File "<stdin>", line 1
+    File "<stdin>", line 1
+    ^
+IndentationError: unexpected indent
+>>>     eggs = {'name': 'Zophie', 'species': 'cat', 'age': '8'}
+  File "<stdin>", line 1
+    eggs = {'name': 'Zophie', 'species': 'cat', 'age': '8'}
+    ^
+IndentationError: unexpected indent
+>>>     ^
+  File "<stdin>", line 1
+    ^
+    ^
+IndentationError: unexpected indent
+>>> IndentationError: unexpected indent
+  File "<stdin>", line 1
+    IndentationError: unexpected indent
+                                 ^
+SyntaxError: invalid syntax
+>>> >>> >>> ham = {'species': 'cat', 'age': '8', 'name': 'Zophie'}
+  File "<stdin>", line 1
+    >>> >>> ham = {'species': 'cat', 'age': '8', 'name': 'Zophie'}
+    ^
+SyntaxError: invalid syntax
+>>>   File "<stdin>", line 1
+  File "<stdin>", line 1
+    File "<stdin>", line 1
+    ^
+IndentationError: unexpected indent
+>>>     >>> ham = {'species': 'cat', 'age': '8', 'name': 'Zophie'}
+  File "<stdin>", line 1
+    >>> ham = {'species': 'cat', 'age': '8', 'name': 'Zophie'}
+    ^
+IndentationError: unexpected indent
+>>>     ^
+  File "<stdin>", line 1
+    ^
+    ^
+IndentationError: unexpected indent
+>>> SyntaxError: invalid syntax
+  File "<stdin>", line 1
+    SyntaxError: invalid syntax
+                         ^
+SyntaxError: invalid syntax
+>>> >>> >>> eggs == ham
+  File "<stdin>", line 1
+    >>> >>> eggs == ham
+    ^
+SyntaxError: invalid syntax
+>>>   File "<stdin>", line 1
+  File "<stdin>", line 1
+    File "<stdin>", line 1
+    ^
+IndentationError: unexpected indent
+>>>     >>> eggs == ham
+  File "<stdin>", line 1
+    >>> eggs == ham
+    ^
+IndentationError: unexpected indent
+>>>     ^
+  File "<stdin>", line 1
+    ^
+    ^
+IndentationError: unexpected indent
+>>> SyntaxError: invalid syntax
+  File "<stdin>", line 1
+    SyntaxError: invalid syntax
+                         ^
+SyntaxError: invalid syntax
+>>> >>> True
+
